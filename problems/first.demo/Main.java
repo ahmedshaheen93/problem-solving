84270db6d154d45e62f47f304d5b9b13ca29b434
ahmed shaheen <ahmedshaheen676@yahoo.com>
2021-10-09 17:07:57 +0000

    O(1) example

--- problems/first.demo/Main.java
+++ problems/first.demo/Main.java
@@ -2,13 +2,14 @@ package first.demo;
 import java.util.*;
 public class Main{
   public static void main(String args[]){
-   List names = new ArrayList<String>();
-   names.add("ahmed");
-   names.add("mohamed");
-   names.add("shaheen");
-   findMyName(names,"ahmed");
+    List names = new ArrayList<String>();
+    names.add("ahmed");
+    names.add("mohamed");
+    names.add("shaheen");
+    findMyName(names,"ahmed");
+    findFirst(names);
   }
-  
+
   /**
   * O(n) example
   */
@@ -18,5 +19,14 @@ public class Main{
           System.out.println("founded in location = "+ i);
         }
       }
+  }
+
+  /**
+  * O(1) example
+  */
+  private static void findFirst(List<String> list){
+    if(!list.isEmpty()){
+      System.out.println(list.get(0));
+    }
   }
 }
