80bb9fdf9a66ad2fd3369ab51ffa23a6abe08ad6
ahmed shaheen <ahmedshaheen93@yahoo.com>
2021-10-10 20:43:42 +0200

    O(a+b) example

--- problems/allpairs/Main.java
+++ problems/allpairs/Main.java
@@ -1,8 +1,11 @@
 package allpairs;
 public class Main{
+
     public static void main(String[] args){
         int[] array ={1,2,3,4,5,6};
-       printAllPairs(array);
+        int[] array2 ={6,5,4,3,2,1};
+       //printAllPairs(array);
+       printAllElements(array,array2);
     }
 
     /**
@@ -14,6 +17,19 @@ public class Main{
             for (int j = 0; j < array.length; j++) {
                 System.out.println(array[i]+" "+ array[j]);
             }
+        }
+    }
+    /**
+     * O(a+b) example
+     * @param array
+     * @param array2
+     */
+    private static void printAllElements(int[]  array ,int[]  array2 ){
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+        for (int i = 0; i < array2.length; i++) {
+            System.out.println(array2[i]);
         }
     }
 }
